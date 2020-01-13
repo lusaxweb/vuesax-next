16fc79e170768e12f008fa7e7d8f85dba943fbf4
ldrovira <luisdanielrovira8@gmail.com>
2020-01-13 13:32:15 +0100

    add input

---
+++ packages/docs/.vuepress/components/Input/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="center">
+    <vs-input>
+      Hola mundo
+    </vs-input>
+  </div>
+</template>
+<script>
+export default {
+  data:() => ({
+    active: 0
+  })
+}
+</script>
--- packages/docs/.vuepress/config.js
+++ packages/docs/.vuepress/config.js
@@ -1,14 +1,14 @@
 module.exports = ctx => ({
   locales: {
-    // The key is the path for the locale to be nested under.
-    // As a special case, the default locale can use '/' as its path.
     '/': {
-      lang: 'English', // this will be set as the lang attribute on <html>
-      title: 'Vuesax',
+      lang: 'English',
+      title: 'VuePress',
+      description: 'Vue-powered Static Site Generator'
     },
     '/es/': {
       lang: 'Español',
       title: 'VuePress',
+      description: 'Vue 驱动的静态网站生成器'
     }
   },
   plugins: [
