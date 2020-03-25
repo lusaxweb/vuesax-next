618843274a9a64b4f3521dd606f7374939cdd45a
Razielini <razielco@gmail.com>
2020-03-25 09:35:02 -0700

    Breadcrumb v0.1
    The first version of the Breadcrumb, like v3.

--- packages/vuesax/src/components/index.ts
+++ packages/vuesax/src/components/index.ts
@@ -13,4 +13,5 @@ export { default as vsRadio } from './vsRadio/Base/index'
 export { default as vsTooltip } from './vsTooltip/Base/index'
 export { default as vsDialog } from './vsDialog/Base/index'
 export { default as vsPagination } from './vsPagination/Base/index'
+export { default as vsBreadcrumb } from './vsBreadcrumb/Base/index'
 // new component slot
---
+++ packages/vuesax/src/components/vsBreadcrumb/Base/vsBreadcrumb.ts
@@ -0,0 +1,78 @@
+import { VNode } from 'vue'
+import { Component, Prop, Watch } from 'vue-property-decorator'
+import VsComponent from '../../../mixins/component'
+import { setColor } from '../../../util/index'
+
+@Component
+export default class VsBreadcrumb extends VsComponent {
+  @Prop({ default: (): any => [], type: Array }) items: string[]
+
+  @Prop({ default: '/', type: String }) separator: string
+
+  @Prop({ default: 'primary', type: String }) color: string
+
+  @Prop({ default: 'left', type: String }) align: string
+
+  @Watch('color')
+  handleColor() {
+    setColor('breadcrumb', this.color , this.$el)
+    this.$el.classList.add('vs-change-color-breadcrumb')
+  }
+
+  mounted() {
+    setColor('breadcrumb', this.color , this.$el)
+  }
+
+  public render(h: any): VNode {
+    const li = this.items.map(((item: any, i: number) => {
+      return h('li', {
+        staticClass: '',
+        class: [
+          item.disabled ? 'disabled-link' : null, item.active ? 'vs-active' : null
+        ]
+      }, [
+        h(item.url ? 'a' : 'span', {
+          staticClass: item.url ? 'vs-breadcrumb-link' : 'vs-breadcrumb--text',
+          class: [
+            item.url ? 'vs-breadcrumb-link' : 'vs-breadcrumb--text'
+          ],
+          attrs: {
+            title: item.url ? item.title : null,
+            href: item.url || '#',
+            style: item.active ? 'color: rgb(var(--vs-color));' : null,
+            'aria-current': item.active ? 'page' : null
+          },
+        }, [ item.title ]),
+        i < (this.items.length - 1) ? h('span', {
+          staticClass: 'separator notranslate vs-breadcrumb--separator',
+          class: [
+            this.separator.length > 1 ? 'material-icons' : null
+          ],
+          attrs: {
+            translate: 'no',
+            'aria-hidden': 'true'
+          },
+        }, [ this.separator ]) : null
+      ])
+    }))
+
+    const ol = h('ol', {
+      staticClass: 'vs-breadcrumb--ol',
+    }, [
+      li
+    ])
+
+    return h('nav', {
+      staticClass: 'vs-breadcrumb',
+      attrs: {
+        ...this.$attrs,
+        'aria-label': 'breadcrumb'
+      },
+      class: [
+        `vs-align-${this.align}`
+      ]
+    }, [
+      this.$slots.default || ol
+    ])
+  }
+}
